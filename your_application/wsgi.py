ac9e89e7f0eed0d92183c42b0f38a6d3387d962d
Harshil089 <harshilbuch05@gmail.com>
2026-03-23 22:39:40 +0530

    fix: expose application callable for render gunicorn module target

--- your_application/wsgi.py
+++ your_application/wsgi.py
@@ -5,4 +5,7 @@ Allows Render start command `gunicorn your_application.wsgi` to boot the app.
 
 from app import app
 
-__all__ = ["app"]
+# Some platforms default to looking for `application` when only a module path is provided.
+application = app
+
+__all__ = ["app", "application"]
